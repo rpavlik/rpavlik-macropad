f2351e3626f4121518129d2ac3b3eb6574d3333d
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-12-07 11:21:39 -0600

    Improve boot menu

--- boot.py
+++ boot.py
@@ -1,9 +1,18 @@
-import storage, usb_cdc
+import storage, usb_cdc, usb_midi
 import board, digitalio
-print("Press and hold top left key to update")
+print("Top-Left for")
+print("Drive Access")
 button = digitalio.DigitalInOut(board.KEY1)
 button.pull = digitalio.Pull.UP
 
 # Disable devices only if button is not pressed.
 if button.value:
+	print("DRIVE INACTIVE")
 	storage.disable_usb_drive()
+	usb_cdc.disable()
+	# usb_midi.disable()
+else:
+	print("DRIVE ACTIVE")
+	print("release now")
+	while not button.value:
+		pass
